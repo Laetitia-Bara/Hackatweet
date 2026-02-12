45335387512931a986e489bc598f93609aea29d5
Thomas MENAGE <thomas.menage@hei.fr>
2026-02-12 15:46:09 +0100

    Connection test modification

--- frontend/components/Tweet.js
+++ frontend/components/Tweet.js
@@ -1,13 +1,37 @@
 import styles from "../styles/Tweet.module.css";
 import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 function Tweet() {
   const [tweetInput, setTweetInput] = useState("");
   const [tweetLength, setTweetLength] = useState(0);
 
+  const token = useSelector((state) => state.user.token);
+
   const handleTweetChange = (tweet) => {
     setTweetInput(tweet);
     setTweetLength(tweet.length);
+  };
+
+  const backendUrl = "http://localhost:3000";
+
+  const handleTweetClick = (tweet) => {
+    fetch(`${backendUrl}/tweet`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        token: token,
+        tweet: tweetInput,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.result) {
+          // fetch route qui affiche tous les tweets
+          setTweetInput("");
+          setTweetLength(0);
+        }
+      });
   };
 
   return (
@@ -25,7 +49,13 @@ function Tweet() {
       </div>
       <div className={styles.tweetActions}>
         <div className={styles.check}>{tweetLength}/280</div>
-        <button className={styles.btnTweet}>Tweet</button>
+        <button
+          className={styles.btnTweet}
+          id="tweet"
+          onClick={() => handleTweetClick(tweetInput)}
+        >
+          Tweet
+        </button>
       </div>
     </div>
   );
